443757ea78c26729b1def0ab8ee748962585f515
GiovanniVolpintesta <john.foxinhead@gmail.com>
2024-06-28 19:12:59 +0200

    Player graphic data for UI
    Player graphic data for UI

--- Assets/Scripts/Data/CharactersData.cs
+++ Assets/Scripts/Data/CharactersData.cs
@@ -2,6 +2,7 @@ using data;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -9,5 +10,19 @@ public class CharactersData : MonoBehaviour
 {
     [SerializeField]
     CharacterConsts[] characterConsts;
+    [SerializeField]
+    string playerCharacterId = "Player";
     public ICollection<CharacterConsts> CharacterConst { get { return characterConsts.AsReadOnlyCollection(); } }
+
+    public CharacterConsts findCharacterConsts(string characterId)
+    {
+        if (characterId == null) characterId = playerCharacterId;
+
+        foreach (CharacterConsts e in CharacterConst)
+        {
+            if (e.Id.Equals(characterId))
+                return e;
+        }
+        return null;
+    }
 }
